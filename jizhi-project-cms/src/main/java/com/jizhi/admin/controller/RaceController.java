bb5cd35062098a4df36779a955c165e10895e267
zhengfy1 <zhengfy1@lenovo.com>
2017-03-13 21:35:38 +0800

    update

--- jizhi-project-cms/src/main/java/com/jizhi/admin/controller/RaceController.java
+++ jizhi-project-cms/src/main/java/com/jizhi/admin/controller/RaceController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jizhi.model.Race;
-import com.jizhi.model.RacePeriods;
 import com.jizhi.service.CouponService;
 import com.jizhi.service.RaceService;
 import com.jizhi.service.UserService;
@@ -71,61 +70,6 @@ public class RaceController {
 			return  AjaxWebUtil.sendAjaxResponse(request, response, true,"添加成功", null);
 		}catch(Exception e) {
 			return  AjaxWebUtil.sendAjaxResponse(request, response, false,"添加失败:"+e.getLocalizedMessage(), null);
-		}
-	}
-	
-	@RequestMapping(value = "periods/list",method=RequestMethod.GET)
-	@ResponseBody
-	public String periodsList(int raceId,int page,int pageSize,HttpServletRequest request, HttpServletResponse response) {
-		try {
-			PageResult races = raceService.getRacePeriodsPageResult(raceId, page, pageSize);
-			return  AjaxWebUtil.sendAjaxResponse(request, response, true,"查询成功", races);
-		}catch(Exception e) {
-			return  AjaxWebUtil.sendAjaxResponse(request, response, false,"查询失败:"+e.getLocalizedMessage(), null);
-		}
-	}
-	
-	@RequestMapping(value = "periods/add",method=RequestMethod.POST)
-	@ResponseBody
-	public String periodsAdd(RacePeriods racePeriods,HttpServletRequest request, HttpServletResponse response) {
-		try {
-			raceService.addRacePeriods(racePeriods);
-			return  AjaxWebUtil.sendAjaxResponse(request, response, true,"添加成功", null);
-		}catch(Exception e) {
-			return  AjaxWebUtil.sendAjaxResponse(request, response, false,"添加失败:"+e.getLocalizedMessage(), null);
-		}
-	}
-	
-	@RequestMapping(value = "periods/update",method=RequestMethod.POST)
-	@ResponseBody
-	public String periodsUpdate(RacePeriods racePeriods,HttpServletRequest request, HttpServletResponse response) {
-		try {
-			raceService.addRacePeriods(racePeriods);
-			return  AjaxWebUtil.sendAjaxResponse(request, response, true,"添加成功", null);
-		}catch(Exception e) {
-			return  AjaxWebUtil.sendAjaxResponse(request, response, false,"添加失败:"+e.getLocalizedMessage(), null);
-		}
-	}
-	
-	@RequestMapping(value = "periods/detail",method=RequestMethod.POST)
-	@ResponseBody
-	public String periodsdetail(int id,HttpServletRequest request, HttpServletResponse response) {
-		try {
-			RacePeriods racePeriods = raceService.queryRacePeridosById(id);
-			return  AjaxWebUtil.sendAjaxResponse(request, response, true,"查询成功", racePeriods);
-		}catch(Exception e) {
-			return  AjaxWebUtil.sendAjaxResponse(request, response, false,"查询失败:"+e.getLocalizedMessage(), null);
-		}
-	}
-	
-	@RequestMapping(value = "periods/delete",method=RequestMethod.POST)
-	@ResponseBody
-	public String periodsdelete(int id,HttpServletRequest request, HttpServletResponse response) {
-		try {
-			raceService.deleteRacePeriods(id);
-			return  AjaxWebUtil.sendAjaxResponse(request, response, true,"删除成功", null);
-		}catch(Exception e) {
-			return  AjaxWebUtil.sendAjaxResponse(request, response, false,"删除失败:"+e.getLocalizedMessage(), null);
 		}
 	}
 }
